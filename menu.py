2de82d645af7549f93ac3fcae5221f2f51db54f8
Garth J. Vander Houwen <garthvh@yahoo.com>
2015-04-15 13:43:51 -0700

    add larger border

--- menu.py
+++ menu.py
@@ -13,7 +13,7 @@ def make_button(text, xpo, ypo, colour):
     font=pygame.font.Font(None,24)
     label=font.render(str(text), 1, (colour))
     screen.blit(label,(xpo,ypo))
-    pygame.draw.rect(screen, cream, (xpo-0,ypo-0,110,35),1)
+    pygame.draw.rect(screen, cream, (xpo-10,ypo-10,110,35),1)
 
 #define function that checks for mouse location
 def on_click():
@@ -84,13 +84,13 @@ logo=pygame.image.load("logo.tiff")
 exit=pygame.image.load("exit.tiff")
 screen.blit(logo,(210,5))
 screen.blit(exit,(200,130))
-pygame.draw.rect(screen, white, (0,0,480,320),4)
+pygame.draw.rect(screen, white, (0,0,480,320),10)
 
 #Add buttons and labels
-make_button("Menu item 1", 20, 20, white)
-make_button("Menu item 2", 20, 70, white)
-make_button("Menu item 3", 20, 120, white)
-make_button("Menu item 4", 20, 170, white)
+make_button("Menu item 1", 40, 40, white)
+##make_button("Menu item 2", 20, 70, white)
+##make_button("Menu item 3", 20, 120, white)
+##make_button("Menu item 4", 20, 170, white)
 
 #While loop to manage touch screen inputs
 while 1:

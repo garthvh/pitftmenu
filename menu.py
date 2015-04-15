24e6969148a20bd6d2525e44640770c85fcf5c0e
Garth J. Vander Houwen <garthvh@yahoo.com>
2015-04-15 13:45:34 -0700

    adjust button size

--- menu.py
+++ menu.py
@@ -13,7 +13,7 @@ def make_button(text, xpo, ypo, colour):
     font=pygame.font.Font(None,24)
     label=font.render(str(text), 1, (colour))
     screen.blit(label,(xpo,ypo))
-    pygame.draw.rect(screen, cream, (xpo-10,ypo-10,110,35),1)
+    pygame.draw.rect(screen, cream, (xpo-10,ypo-10,200,60),1)
 
 #define function that checks for mouse location
 def on_click():

67eabe9ebef4154e3d49a147ed453229c261c70f
Garth J. Vander Houwen <garthvh@yahoo.com>
2015-04-18 11:20:29 -0700

    add 6th button

--- menu_touchpi.py
+++ menu_touchpi.py
@@ -30,21 +30,24 @@ def on_touch():
     # get the position that was touched
     touch_pos = (pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
     #  x_min                 x_max   y_min                y_max
-    # button 3 event
+    # button 1 event
     if 30 <= touch_pos[0] <= 240 and 105 <= touch_pos[1] <=160:
             button(1)
-    # button 4 event
+    # button 2 event
     if 260 <= touch_pos[0] <= 470 and 105 <= touch_pos[1] <=160:
             button(2)
-    # button 5 event
+    # button 3 event
     if 30 <= touch_pos[0] <= 240 and 180 <= touch_pos[1] <=235:
             button(3)
-    # button 6 event
+    # button 4 event
     if 260 <= touch_pos[0] <= 470 and 180 <= touch_pos[1] <=235:
             button(4)
-    # button 8 event
-    if 260 <= touch_pos[0] <= 470 and 255 <= touch_pos[1] <=310:
+    # button 5 event
+    if 30 <= touch_pos[0] <= 240 and 255 <= touch_pos[1] <=310:
             button(5)
+    # button 6 event
+    if 260 <= touch_pos[0] <= 470 and 255 <= touch_pos[1] <=310:
+            button(6)
 
 # Get Your External IP Address
 def get_ip():
@@ -119,7 +122,6 @@ def button(number):
         pygame.quit()
         restart()
         sys.exit()
-
     if number == 4:
         # shutdown
         screen.fill(black)
@@ -130,7 +132,7 @@ def button(number):
         pygame.quit()
         shutdown()
         sys.exit()
-    if number == 5:
+    if number == 6:
         # Wifi Settings
         screen.fill(black)
         font=pygame.font.Font(None,72)
@@ -174,9 +176,8 @@ make_button("    Terminal", 260, 105, 55, 210, blue)
 # Third Row buttons 5 and 6
 make_button("      Reboot", 30, 180, 55, 210, blue)
 make_button("    Shutdown", 260, 180, 55, 210, blue)
-# Fourth Row Label
-#make_label("Current IP: " +  get_ip(), 20, 255, 48, blue)
-#make_label("IP:" +  get_ip(), 10, 255, 42, blue)
+# Fourth Row Buttons
+make_button("Empty Button", 30, 255, 55, 210, blue)
 make_button("WiFi Settings", 260, 255, 55, 210, blue)
 
 

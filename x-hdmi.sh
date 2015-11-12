720ec7ff9753f08d28b2fe6c6e0059f96a284178
root <root@WOPR-RasPi-IV.(none)>
2015-11-12 13:59:05 +1100

    added second page

--- x-hdmi.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo Starting X
-sudo -u pi FRAMEBUFFER=/dev/fb0 startx
-python /home/pi/pitftmenu/menu_kali-1.py &
--- x-tft.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo Starting X
-sudo -u pi FRAMEBUFFER=/dev/fb1 startx
-python /home/pi/pitftmenu/menu_kali-1.py &
